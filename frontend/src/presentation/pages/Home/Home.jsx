04ee009dbe1ad65d7db39b3abbfebe2977e1b67f
Bedknobs <noeliareysilva@gmail.com>
2024-04-04 11:57:09 +0200

    Added the pages folder and the Home page.

---
+++ frontend/src/presentation/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import HeaderHome from "../../components/HeaderHome/HeaderHome";
+import Slider from "../../components/Slider/Slider";
+
+export default function Home() {
+  return (
+    <>
+      <HeaderHome />
+      <Slider />
+    </>
+  );
+}

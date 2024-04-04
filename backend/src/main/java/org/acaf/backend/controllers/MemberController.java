ee9e8da7e7256620600e0edf7664c3515bd25196
enolsurfer <39172327+enolsurfer@users.noreply.github.com>
2024-04-04 11:52:05 +0200

    Members añadido

--- backend/src/main/java/org/acaf/backend/controllers/MemberController.java
+++ backend/src/main/java/org/acaf/backend/controllers/MemberController.java
@@ -1,5 +1,31 @@
 package org.acaf.backend.controllers;
 
+import java.util.List;
+
+import org.acaf.backend.models.Member;
+import org.acaf.backend.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/members")
+
 public class MemberController {
-    
+
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping
+    public List<Member> findAll() {
+        return memberService.findAll();
+    }
+
+    @PostMapping
+    public Member create(@RequestBody Member member) {
+        return memberService.save(member);
+    }
 }
--- backend/src/main/java/org/acaf/backend/service/MemberService.java
+++ backend/src/main/java/org/acaf/backend/service/MemberService.java
@@ -1,5 +1,23 @@
 package org.acaf.backend.service;
 
+import java.util.List;
+
+import org.acaf.backend.models.Member;
+import org.acaf.backend.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MemberService {
-    
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public Member save(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
 }

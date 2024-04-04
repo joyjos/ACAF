138b44fd1dd106d0321e79de7e133b0152547639
Bedknobs <noeliareysilva@gmail.com>
2024-04-04 14:15:00 +0200

    Added InfoHome component

--- frontend/src/presentation/pages/Home/Home.jsx
+++ frontend/src/presentation/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import Slider from "../../components/Slider/Slider";
 import Footer from "../../components/Footer/Footer";
+import InfoHome from "../../components/InfoHome/InfoHome";
 
 export default function Home() {
-
   return (
     <>
       <HeaderHome />
       <Slider />
+      <InfoHome />
       <Footer />
     </>
   );

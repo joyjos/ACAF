ffdd85188f88953627561d7107ad98c71ef080a3
Bedknobs <noeliareysilva@gmail.com>
2024-04-04 12:32:51 +0200

    The slider is done, the style is missing

--- frontend/src/presentation/pages/Home/Home.jsx
+++ frontend/src/presentation/pages/Home/Home.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import Slider from "../../components/Slider/Slider";
 
 export default function Home() {
+  const [isSticky, setIsSticky] = useState(false);
+  const offset = window.innerHeight / 2; // Use dynamic value based on viewport height
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      setIsSticky(scrollTop > offset);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <>
-      <HeaderHome />
+      <HeaderHome isSticky={isSticky} />
       <Slider />
     </>
   );

6509c42cadc29c600e9754429087cd8c311f197a
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-04-04 12:34:42 +0200

    Add component footer

--- frontend/src/presentation/pages/Home/Home.jsx
+++ frontend/src/presentation/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import Slider from "../../components/Slider/Slider";
+import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
   return (
     <>
       <HeaderHome />
       <Slider />
+      <Footer />
     </>
   );
 }

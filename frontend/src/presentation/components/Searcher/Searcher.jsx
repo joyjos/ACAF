79e541f9cb45c23a90826c9060d0788e4e1c4804
chanita23 <sanamapeca@gmail.com>
2024-04-05 13:56:45 +0100

    inicial Searcher css

---
+++ frontend/src/presentation/components/Searcher/Searcher.jsx
@@ -0,0 +1,25 @@
+import './Searcher.css';
+
+import PersonIcon from '@mui/icons-material/Person';
+const UsersSearcher = () => {
+
+  return (
+    <section className="userSearchContainer">
+      <div className='userSearchContainer__Field'>
+        <div className="userButton">
+        <PersonIcon className='userIcon' />
+        </div>
+      
+        <input
+          type="search"
+          className="userSearchContainer__Field-Input"
+          placeholder=" Buscar socio por nombre, apellido, nº de socio"
+        />
+        <button className="userSearchContainer__Field-Button">
+          <img src="/src/assets/searchIcon.svg" alt="búsqueda icono" />
+        </button>        
+      </div>
+    </section>
+  )
+}
+export default UsersSearcher;
--- frontend/src/presentation/pages/Home/Home.jsx
+++ frontend/src/presentation/pages/Home/Home.jsx
@@ -2,14 +2,19 @@ import HeaderHome from "../../components/HeaderHome/HeaderHome";
 import Slider from "../../components/Slider/Slider";
 import Footer from "../../components/Footer/Footer";
 import InfoHome from "../../components/InfoHome/InfoHome";
+import UsersSearcher  from "../../components/Searcher/Searcher";
 
 export default function Home() {
   return (
     <>
+      
       <HeaderHome />
       <Slider />
       <InfoHome />
+      <UsersSearcher />
       <Footer />
-    </>
+     </>
   );
-}
+}  
+      
+   

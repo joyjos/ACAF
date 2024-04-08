4a661b124a30f77a23e7075c4cddac44046019b7
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-04-08 11:45:00 +0200

    Add h1 for PartnertsLIst

--- frontend/src/presentation/pages/PartnersList/PartnersList.jsx
+++ frontend/src/presentation/pages/PartnersList/PartnersList.jsx
@@ -1,12 +1,11 @@
 import "./PartnersList.css"
 import Searcher from "../../components/Searcher/Searcher";
-
-
-
+import HeaderHome from "../../components/HeaderHome/HeaderHome";
 
 const PartnersList = () => {
   return (
     <div>
+      <HeaderHome />
       <h1>LISTADO DE SOCIOS</h1>
       <Searcher />
     </div>

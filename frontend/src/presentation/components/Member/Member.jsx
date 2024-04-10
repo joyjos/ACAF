209696ee3cd781e895ec379d066fb9c9c5fa2455
Bedknobs <noeliareysilva@gmail.com>
2024-04-10 09:16:54 +0200

    fixed member style

--- frontend/src/presentation/components/Member/Member.jsx
+++ frontend/src/presentation/components/Member/Member.jsx
@@ -1,23 +1,21 @@
 import "./Member.css";
-import { useDataContext } from '../../../middleware/context/DataContext';
+import { useDataContext } from "../../../middleware/context/DataContext";
 
 function Member() {
   const { memberList } = useDataContext();
 
-  console.log(memberList)
+  console.log(memberList);
   return (
-    <div className="member">
+    <div>
       {memberList.map((member) => (
-        <div key={member.id}>
+        <div key={member.id} className="member">
           <p>{member.id}</p>
           <p>{member.name}</p>
           <p>{member.category}</p>
           <p className="bold">{member.cuantity}€</p>
-       </div>
-      )
-      )
-      }
-      </div>
-  )
+        </div>
+      ))}
+    </div>
+  );
 }
 export default Member;

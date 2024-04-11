4661726e6b84088f1ab60443f6d777e48681c6a3
Bedknobs <noeliareysilva@gmail.com>
2024-04-11 11:30:40 +0200

    Added update method

--- frontend/src/presentation/components/Member/Member.jsx
+++ frontend/src/presentation/components/Member/Member.jsx
@@ -1,5 +1,7 @@
 import "./Member.css";
 import PropTypes from "prop-types";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import DeleteForeverOutlinedIcon from "@mui/icons-material/DeleteForeverOutlined";
 
 function Member({ memberList }) {
   return (
@@ -9,14 +11,24 @@ function Member({ memberList }) {
         <h2>Nombre</h2>
         <h2>Categoría</h2>
         <h2>Aportación</h2>
+        <EditOutlinedIcon style={{ fontSize: 30, opacity: 0 }} />
+        <DeleteForeverOutlinedIcon style={{ fontSize: 30, opacity: 0 }} />
       </section>
       <div>
-        {memberList.map((member) => (
-          <div key={member.id} className="member">
-            <p>{member.id}</p>
-            <p>{member.name}</p>
-            <p>{member.category}</p>
-            <p className="bold">{member.cuantity}€</p>
+        {memberList.sort((a, b) => a.id - b.id).map((member) => (
+          <div className="memberMapContainer" key={member.id}>
+            <div className="member">
+              <p>{member.id}</p>
+              <p>{member.name}</p>
+              <p>{member.category}</p>
+              <p className="bold">{member.cuantity}€</p>
+            </div>
+            <div>
+              <EditOutlinedIcon style={{ fontSize: 30, cursor: "pointer" }} />
+              <DeleteForeverOutlinedIcon
+                style={{ fontSize: 30, cursor: "pointer" }}
+              />
+            </div>
           </div>
         ))}
       </div>

1bd82f133fe3feaecab29f8d16e29945cefdd099
Marta <martalarena2002@gmail.com>
2024-04-15 12:42:22 +0200

    back button adminPage

--- frontend/src/presentation/adminComponents/header/AdminHeader.jsx
+++ frontend/src/presentation/adminComponents/header/AdminHeader.jsx
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import arrowBack from "../../assets/arrowBack.png";
 import "./AdminHeader.css"
 
 export default function AdminHeader() {
+  const navigate = useNavigate();
+
+  // Call this function when you want to go back
+  const goBack = () => {
+    navigate(-1);
+  };
   return (
     <header className="headerAdmin">
-      <img src={arrowBack} alt="arrow Back" />
+      {/* <button onClick={goBack}> */}
+      <img onClick={goBack} src={arrowBack} alt="arrow Back" />
+
+      {/* </button> */}
       <Link to="/">
       CERRAR SESIÓN
       </Link>

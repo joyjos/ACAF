86bd0630cd960e57cf561ed9ddde3b77812e7e3e
Marta <martalarena2002@gmail.com>
2024-04-15 12:59:03 +0200

    back to home header

--- frontend/src/presentation/components/Header/Header.jsx
+++ frontend/src/presentation/components/Header/Header.jsx
@@ -1,9 +1,14 @@
 import { Link } from "react-router-dom";
+import acaf from "../../assets/ACAF.svg"
 import "./Header.css";
 
 export default function Header() {
+
   return (
     <div className="headerContainer">
+      <Link to="/">
+        <img src={acaf} alt="logo" />
+      </Link>
       <section className="headerHomeOptions">
         <Link to="/aboutus">
           <p>Quiénes somos</p>

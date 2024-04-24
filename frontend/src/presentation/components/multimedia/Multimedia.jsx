41ba31666b1784409b57933d89e0fbaa0e813bd7
Marta <martalarena2002@gmail.com>
2024-04-24 11:22:17 +0200

    multimedia page done

--- frontend/src/presentation/components/multimedia/Multimedia.jsx
+++ frontend/src/presentation/components/multimedia/Multimedia.jsx
@@ -3,25 +3,24 @@ import playa from "../../assets/playa de la arena.jpg";
 import "./Multimedia.css";
 
 export default function Multimedia() {
+  const multimediaImg =  Array.from({ length: 12 }, (_, index) => {
+    return index % 2 === 0 ? Foguera : playa;
+  });
+
   return (
     <main className="containerMultimedia">
       <p>Multimedia</p>
       <div>
-        <section>
-          <img src={Foguera} alt="Foguera" />
-        </section>
-
-        <img src={playa} alt="playa" />
-        <img src={Foguera} alt="Foguera" />
-        <img src={playa} alt="playa" />
-        <img src={Foguera} alt="Foguera" />
-        <img src={playa} alt="playa" />
-        <img src={Foguera} alt="Foguera" />
-        <img src={playa} alt="playa" />
-        <img src={Foguera} alt="Foguera" />
-        <img src={playa} alt="playa" />
-        <img src={Foguera} alt="Foguera" />
-        <img src={playa} alt="playa" />
+        {multimediaImg.map((img, index) => (
+          <article key={index} className="containerImage">
+            <section>
+              <img src={img} alt={index % 2 === 0 ? "Foguera" : "playa"}/>
+            </section>
+            <section className="containerText">
+              <p>{index % 2 === 0 ? "Hoguera de San Juan" : "Playa"}</p>
+            </section>
+          </article>
+        ))};
       </div>
     </main>
   );

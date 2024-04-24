acd24e97baee3fec4ad1e93b2ea26fe3f4d66eed
Marta <martalarena2002@gmail.com>
2024-04-24 15:14:14 +0200

    activities delete

--- frontend/src/middleware/router/AppRouter.jsx
+++ frontend/src/middleware/router/AppRouter.jsx
@@ -6,27 +6,24 @@ import AdminRouter from "./AdminRouter";
 import { DataProvider } from "../context/DataContext";
 import PartnersList from "../../presentation/pages/PartnersList/PartnersList";
 import { Login } from "../../presentation/adminComponents/login/Login";
-import Activities from "../../presentation/pages/Activities/Activities";
 import News from "../../presentation/pages/News/News";
 import MultimediaPage from "../../presentation/pages/multimedia/MultimediaPage";
 
 const AppRouter = () => {
   return (
     <DataProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="homeAdmin/partners" element={<PartnersList />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/activities" element={<Activities />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/multimedia" element={<MultimediaPage/>} />
-      </Routes>
-      <AdminRouter/>
-    </BrowserRouter>
-  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="homeAdmin/partners" element={<PartnersList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/news" element={<News />} />
+          <Route path="/multimedia" element={<MultimediaPage />} />
+        </Routes>
+        <AdminRouter />
+      </BrowserRouter>
     </DataProvider>
   );
 };

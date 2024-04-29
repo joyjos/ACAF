add8f5f83073266d8f6e87e805e1239b93a7795e
VicSDN <151613334+VicSDN@users.noreply.github.com>
2024-04-29 11:17:08 +0200

    cconfiguracion

--- backend/src/main/java/org/acaf/backend/BackendApplication.java
+++ backend/src/main/java/org/acaf/backend/BackendApplication.java
@@ -2,9 +2,7 @@ package org.acaf.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:application-local.properties")
 @SpringBootApplication
 public class BackendApplication {
 
